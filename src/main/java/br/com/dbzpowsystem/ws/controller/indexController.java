d3738e246fef5b320ff221363547aeeafce51d3f
ThyagoMac <thyago.cormick@gmail.com>
2018-09-30 21:00:34 -0300

    save

---
+++ src/main/java/br/com/dbzpowsystem/ws/controller/indexController.java
@@ -0,0 +1,13 @@
+package br.com.dbzpowsystem.ws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+	
+	@RequestMapping("/home")
+	public String irParaIndex() {
+		return "index";
+	}
+}
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+//<![CDATA[
+function carregarDados() {
+	  var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	     document.getElementById("demo").innerHTML = this.responseText;
+	    }
+	  };
+	  xhttp.open("GET", "http://localhost:8000/personagens", true);
+	  xhttp.send();
+	}
+carregarDados();
+//]]>
